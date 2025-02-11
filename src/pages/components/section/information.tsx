a1d973bef96e05be8ab24c967a3b9c370d36bea0
reykialmadani <reykialmadani09@gmail.com>
2025-02-11 16:17:33 +0700

    Front-End: fixasi infomationPage , Bug: gap card

--- src/pages/components/section/information.tsx
+++ src/pages/components/section/information.tsx
@@ -6,61 +6,63 @@ const Informasi = () => {
     <section className="bg-gray-50 py-12 px-6 sm:px-12">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Informasi Terkini Tentang Kami</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
 
-          {/* Card 1 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto">
-            <Image
-              className="w-full h-64 object-cover"
-              src="https://bankabdi.co.id/storage/post-images/MHYRuxSlECwR5xpaybz4nSthGcNQ9uCeUlG3s2vb.png"
-              alt="Informasi Terkini"
-              width={640} 
-              height={256}
-            />
-            <div className="p-6 flex flex-col h-full">
-              <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
-              <p className=" font-bold text-black text-sm mb-4 flex-grow">
-                Tips Menyusun Anggaran Penjualan, Biar Cuan Makin Mksimal!
-              </p>
-              <a href="#" className="text-indigo-600 hover:text-indigo-800 mt-auto">Selengkapnya</a>
+            {/* Card 1 */}
+            <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto flex flex-col">
+              <Image
+                className="w-full h-[70%] object-cover"
+                src="https://bankabdi.co.id/storage/post-images/MHYRuxSlECwR5xpaybz4nSthGcNQ9uCeUlG3s2vb.png"
+                alt="Informasi Terkini"
+                width={640} 
+                height={278} 
+              />
+              <div className="p-6 flex flex-col flex-grow">
+                <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
+                <p className="font-bold text-black text-sm mb-2">
+                  Tips Menyusun Anggaran Penjualan, Biar Cuan Makin Maksimal!
+                </p>
+                <a href="#" className="text-sm text-gray-300 hover:text-indigo-800">Baca Selengkapnya</a>
+              </div>
             </div>
-          </div>
+
 
           {/* Card 2 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto flex flex-col">
             <Image
-              className="w-full h-64 object-cover"
+              className="w-full h-[70%] object-cover"
               src="https://bankabdi.co.id/storage/post-images/mIDthWsAMu65K7Le8Q5qGPA5qlELd6FUXxO89Pfr.png"
               alt="Informasi Terkini"
               width={640} 
-              height={256}
+              height={278}
             />
-            <div className="p-6 flex flex-col h-full">
-            <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
-            <p className=" font-bold text-black text-sm mb-4 flex-grow">
+            <div className="p-6 flex flex-col flex-grow">
+              <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
+              <p className="font-bold text-black text-sm mb-2 ">
                 Tips Menyusun Anggaran Penjualan, Biar Cuan Makin Mksimal!
               </p>
-              <a href="#" className="text-indigo-600 hover:text-indigo-800 mt-auto">Selengkapnya</a>
+              <a href="#" className="text-sm text-gray-300 hover:text-indigo-800 ">Baca Selengkapnya</a>
             </div>
           </div>
 
           {/* Card 3 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden w-[288px] h-[556px] mx-auto flex flex-col">
             <Image
-              className="w-full h-64 object-cover"
+              className="w-full h-[70%] object-cover"
               src="https://bankabdi.co.id/storage/post-images/iNWDdyCNwdPwiMdXuJF4Jc04peujGAnYHYBIY11J.png"
               alt="Informasi Terkini"
               width={640} 
               height={256}
             />
-            <div className="p-6 flex flex-col h-full">
-            <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
-            <p className=" font-bold text-black text-sm mb-4 flex-grow">
+            <div className="p-6 flex flex-col flex-grow">
+              <h3 className="text-sm text-gray-400 mb-2">Nasabah Bank ABDI</h3>
+              <p className="font-bold text-black text-sm mb-2 ">
                 Tips Menyusun Anggaran Penjualan, Biar Cuan Makin Mksimal!
               </p>
-              <a href="#" className="text-indigo-600 hover:text-indigo-800 mt-auto">Selengkapnya</a>
+              <a href="#" className="text-sm text-gray-300 hover:text-indigo-800">Baca Selengkapnya</a>
             </div>
           </div>
+
         </div>
       </div>
     </section>
